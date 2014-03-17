908510236bf116fb56075e42f89e3d8d392b1b8c
Aaron Kravitz <aaron@dn0a22519f.sunet>
2014-03-16 17:38:19 -0700

    animations

--- birdgame.py
+++ birdgame.py
@@ -7,6 +7,23 @@ import random
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
+def load_sliced_sprites(w, h, rows, filename):
+    '''
+    Specs :
+        Master can be any height.
+        Sprites frames width must be the same width
+        Master width must be len(frames)*frame.width
+    Assuming you ressources directory is named "ressources"
+    '''
+    images = []
+    master_image = pygame.image.load(filename)
+
+    master_width, master_height = master_image.get_size()
+    for row in xrange(rows):
+        for i in xrange(int(master_width/w)):
+            images.append(master_image.subsurface((i*w,row*h,w,h)))
+
+    return images
 
 class Bird(pygame.sprite.Sprite):
 
@@ -16,8 +33,11 @@ class Bird(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self._images = []
+        self._images.append(pygame.image.load('plane_1.png'))
+        self._images.append(pygame.image.load('plane_2.png'))
 
-        self.image = pygame.image.load('plane_1.png')
+        self.image = self._images[0]
 
         self.rect = self.image.get_rect()
 
@@ -26,7 +46,18 @@ class Bird(pygame.sprite.Sprite):
 
         self._space_pressed = False
 
-    def update(self):
+        self._delay = 1000/30 # change to global
+        self._last_update = 0
+        self._frame = 0
+
+
+    def update(self, t):
+        if t - self._last_update > self._delay:
+            self._frame += 1
+            if self._frame >= len(self._images): self._frame = 0
+            self.image = self._images[self._frame]
+            self._last_update = t
+
         if self._space_pressed:
             self.y_velocity = -10
         else:
@@ -37,6 +68,8 @@ class Bird(pygame.sprite.Sprite):
             self.rect.y = 0
         if self.rect.y > SCREEN_HEIGHT - self.rect.height:
             self.rect.y = SCREEN_HEIGHT - self.rect.height
+
+
 
 
 class Coin(pygame.sprite.Sprite):
@@ -84,17 +117,29 @@ class Explode(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.image.load('Explode.png')
+        self._images = load_sliced_sprites(48, 48, 2, 'explosions.png')
 
+        self.image = pygame.image.load('Explode.png')
         self.rect = self.image.get_rect()
 
         self.rect.x = SCREEN_WIDTH
         self.rect.y = random.random() * SCREEN_HEIGHT
+        self._exploding = False
 
         self.x_velocity = -10
+        self._delay = 1000/30 # change to global
+        self._last_update = 0
+        self._frame = 0
 
-    def update(self):
-        self.rect.x += self.x_velocity
+    def update(self, t):
+        if self._exploding and t - self._last_update > self._delay:
+            self._frame += 1
+            if self._frame >= len(self._images): self._frame = len(self._images) - 1
+            self.image = self._images[self._frame]
+            self._last_update = t
+
+        if not self._exploding:
+            self.rect.x += self.x_velocity
 
 class App:
 
@@ -136,7 +181,7 @@ class App:
                 self._bird._space_pressed = True
 
     def on_loop(self):
-        self._bird_group.update()
+        self._bird_group.update(pygame.time.get_ticks())
         if random.randrange(1, 11) == 1:
             coin = Coin()
             gameover = GameOver()
@@ -145,23 +190,24 @@ class App:
         if random.randrange(1, 11) == 1:    
             explode = Explode()
             self._explode_group.add(explode)
+        
+        self._explode_group.update(pygame.time.get_ticks())
 
         if self._alive:
             self._coin_group.update()
-            self._explode_group.update()
 
-        for coin in self._coin_group.sprites():
-            if coin.rect.colliderect(self._bird.rect):
-                self._coin_group.remove(coin)
-            if coin.rect.x < 0 - coin.rect.width:
-                self._coin_group.remove(coin)
+            for coin in self._coin_group.sprites():
+                if coin.rect.colliderect(self._bird.rect):
+                    self._coin_group.remove(coin)
+                if coin.rect.x < 0 - coin.rect.width:
+                    self._coin_group.remove(coin)
 
-        for explode in self._explode_group.sprites():
-            if explode.rect.colliderect(self._bird.rect):
-                self._alive = False
-                add(gameover)
-            if explode.rect.x < 0 - explode.rect.width:
-                self._explode_group.remove(explode)        
+            for explode in self._explode_group.sprites():
+                if explode.rect.colliderect(self._bird.rect):
+                    self._alive = False
+                    explode._exploding = True
+                if explode.rect.x < 0 - explode.rect.width:
+                    self._explode_group.remove(explode)        
 
 
     def on_render(self):
